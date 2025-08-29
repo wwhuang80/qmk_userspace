2ff0e917e64de00b9f8566873d65cb07648a25d0
William Huang <wwhuang@ucdavis.edu>
2025-08-29 08:49:55 -0700

    updated working encoder mapping

---
+++ keyboards/splitkb/halcyon/lily58/keymaps/wwhuang80_hlc/config.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 250
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+// Mouse key speed and acceleration.
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY          0
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       16
+#undef MOUSEKEY_WHEEL_DELAY
+#define MOUSEKEY_WHEEL_DELAY    0
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED      6
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX    64
---
+++ keyboards/splitkb/halcyon/lily58/keymaps/wwhuang80_hlc/rules.mk
@@ -0,0 +1,16 @@
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
+TAP_DANCE_ENABLE = no
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+SRC += chordmap.c
+# This adds module functionality to your keyboard (files found in users/halcyon_modules)
+
+USER_NAME := halcyon_modules
+
+
